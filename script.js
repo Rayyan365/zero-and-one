5aaa195e26dd7f14f286502a5e2ca07718b9e2d8
Rayyan365 <rayanabdallah09@gamil.com>
2023-08-19 07:38:33 -0700

    zero-and-one

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Good Luck 💪🏾
+// let {"zero" = 0};
+// let {"one"= 1};
+var stringArray = ["one", "zero", "zero", "one", "zero"];
+  
+
+var numberArray = [];
+
+length = stringArray.length;
+  
+
+
+for (var i = 0; i < length; i++)
+  
+  
+    numberArray.push(parseInt(stringArray[i]));
+  
+console.log(numberArray);
